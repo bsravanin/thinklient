77079d0ff9d8a6a7e9cafc0bf21ea7d1409be700
Sravan Bhamidipati <bsravanin@gmail.com>
2017-06-11 16:33:21 -0400

    fix: Style the discard button
    The discard button is now "rust" colored, positioned to the left
    of the "post" button. The way the style has been defined is
    brittle (based on trial and error), especially the 'marginTop'
    property, and may need further refinement in the future.

--- src/encourage-discard/encourage-discard.ts
+++ src/encourage-discard/encourage-discard.ts
@@ -6,19 +6,30 @@ export const encourageDiscard = () => {
         console.error(postBoxes)
     } else {
         const postBox = postBoxes[0]
-        const btn = document.createElement('BUTTON')
-        btn.id = 'thinklient-discard'
-        btn.textContent = 'Discard'
-        btn.onclick = function() {
+        const discard_btn = document.createElement('BUTTON')
+        discard_btn.id = 'thinklient-discard'
+        discard_btn.textContent = 'Discard'
+        discard_btn.style.backgroundColor = '#B7410E'
+        discard_btn.style.border = 'none'
+        discard_btn.style.color = 'white'
+        discard_btn.style.height = '24px'
+        discard_btn.style.marginTop = '8px'
+        discard_btn.onclick = function() {
             const dismissButton = <HTMLElement> document.querySelectorAll('[aria-label="Dismiss"]')[0]
             dismissButton.click()
         }
+        const discard_div = document.createElement('DIV')
+        discard_div.appendChild(document.createElement('span'))
+        discard_div.appendChild(discard_btn)
+        discard_div.style.cssFloat = 'left'
 
         let observer = new MutationObserver(function () {
-            const lfloat = postBox.getElementsByClassName('lfloat')[0]
-            if (lfloat && lfloat.getElementsByTagName('button').length == 0) {
+            const rfloat = postBox.getElementsByClassName('rfloat')[0]
+            if (rfloat && rfloat.getElementsByTagName('button').length == 1) {
                 console.log('Attaching the discard button to the post box.')
-                lfloat.appendChild(btn)
+                const post_btn = <HTMLElement> rfloat.childNodes[0]
+                post_btn.style.cssFloat = 'left'
+                rfloat.insertBefore(discard_div, post_btn)
             }
         })
 
